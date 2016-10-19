7d69dbfe681490cd74f038a0c713a14ecfc99a22
Austin Hughes <austindavidhughes@gmail.com>
2016-10-19 17:07:35 -0600

    Assembler project

---
+++ assembler.py
@@ -0,0 +1,78 @@
+import sys
+
+
+def node():
+    return {
+        'in_nodes': [],
+        'out_nodes': []
+    }
+
+nodes = {}
+def add_node(name):
+    if name not in nodes:
+        nodes[name] = node()
+
+def add_nodes(head, tail):
+    add_node(head)
+    add_node(tail)
+    nodes[head]['out_nodes'].append(tail)
+    nodes[tail]['in_nodes'].append(head)
+
+# look at last item in contig - if it's a branching node, we're done, otherwise keep going
+def continue_contig(contig):
+    end = contig[-1]
+    if nodes[end]['branching']:
+        return contig[0] + ''.join(i[-1] for i in contig[1:])
+    else:
+        contig.append(edge_dict[end][0])
+        return continue_contig(contig)
+
+with open(sys.argv[1]) as file:
+    reads = [line.rstrip() for line in file.readlines() if not line.startswith('>')]
+    k = int(sys.argv[2])
+
+    kmers = []
+    for read in reads:
+        for i in range(len(read)):
+            kmer = read[i: i + k]
+            if len(kmer) == k:
+                kmers.append(kmer)
+
+    edges = {}
+    for kmer in kmers:
+        prefix = kmer[:-1]
+        suffix = kmer[1:]
+        if prefix not in edges:
+            edges[prefix] = []
+        # if suffix not in edges[prefix]:
+        #     edges[prefix].append(suffix)
+        edges[prefix].append(suffix)
+
+    for prefix in sorted(edges.keys()):
+        print prefix + ' -> ' + ','.join(sorted(edges[prefix]))
+
+    edge_dict = {}
+    for kmer in kmers:
+        head = kmer[:len(kmer) - 1]
+        tail = kmer[1:]
+        add_nodes(head, tail)
+        if head not in edge_dict:
+            edge_dict[head] = []
+        edge_dict[head].append(tail)
+
+    for n in nodes:
+        node = nodes[n]
+        node['branching'] = not (len(node['out_nodes']) == 1 and len(node['in_nodes']) == 1)
+
+    # go through each branching node and start a new contig
+    branching_nodes = [node for node in nodes if nodes[node]['branching']]
+    contigs = []
+    for node in branching_nodes:
+        if node in edge_dict:
+            for connected_node in edge_dict[node]:
+                contig = [node, connected_node]
+                contig = continue_contig(contig)
+                contigs.append(contig)
+
+    print ''
+    print '\n'.join(set(contigs))
