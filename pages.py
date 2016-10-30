7375d25ef650112a8f74f9843e2dc588a3d8b99e
Austin Hughes <austindavidhughes@gmail.com>
2016-10-29 21:32:50 -0600

    Removed html folder and moved pages into pages/ directory. Each gets its own folder with an index.html. to clean up our urls

--- pages.py
+++ pages.py
@@ -1,11 +1,17 @@
 import json
 import os, shutil
 
+dev = False
+if dev:
+    path = '/pages/'
+else:
+    path = '/Bio365-Group-Project/pages/'
+
 
 def get_sidebar(sections):
     links = []
     for section in sections:
-        links.append('<a href="/Bio365-Group-Project/html/{1}.html">{0}</a>'.format(section[0], section[1]))
+        links.append('<a href="{path}{1}">{0}</a>'.format(section[0], section[1], path=path))
     return '''
     <div id="sidebar">
         {}
@@ -26,7 +32,7 @@ def get_content(content_list):
                 </div>'''.format(content["video"]))
             elif 'picture' in content:
                 html.append('''<div class="picture">
-                    <img src="../pics/{}">
+                    <img src="../../pics/{}">
                 </div>'''.format(content["picture"]))
     return '\n'.join(html)
 
@@ -40,8 +46,8 @@ def get_footer(prev_page, next_page):
     def get_button_html(id, text, page_num):
         if page_num:
             return '''
-                <button id="{0}" onclick="location.href='/Bio365-Group-Project/html/{2}.html'">{1}</button>
-            '''.format(id, text, page_num)
+                <button id="{0}" onclick="location.href='{path}{2}'">{1}</button>
+            '''.format(id, text, page_num, path=path)
         else:
             return ''
 
@@ -65,7 +71,7 @@ def generate_html(sections, page, prev, next):
 <html>
 	<head>
 		<title>Bioinformatics</title>
-		<link rel="stylesheet" href="../site.css">
+		<link rel="stylesheet" href="../../site.css">
 	</head>
 	<body>
 		{sidebar}
@@ -82,25 +88,52 @@ def generate_html(sections, page, prev, next):
 
 
 def delete_prev_html():
-    folder = os.getcwd() + '/html'
+    folder = os.getcwd() + '/pages'
     for the_file in os.listdir(folder):
         file_path = os.path.join(folder, the_file)
         try:
             if os.path.isfile(file_path):
                 os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
         except Exception as e:
             print(e)
 
+# delete prevous index.html and create with proper linked path
+def recreate_main_index_html():
+    try:
+        os.unlink(os.getcwd() + '/index.html')
+    except:
+        pass
+    index_file = open(os.getcwd() + '/index.html', 'w')
+    html = '''
+    <html>
+        <head>
+            <title>Bioinformatics</title>
+            <meta http-equiv="refresh" content="0; url={path}1" />
+        </head>
+        <body>
+        </body>
+    </html>
+'''.format(path=path)
+    index_file.write(html)
+    index_file.close()
+
 def save_html_file(html, num):
-    file = open('html/{}.html'.format(num), 'w')
+    os.mkdir('pages/{}'.format(num))
+    file = open('pages/{}/index.html'.format(num), 'w')
     file.write(html)
     file.close()
 
 
+# clear previous pages
 delete_prev_html()
+recreate_main_index_html()
 
+if dev:
+    print 'GENERATING HTML FILES FOR LOCAL DEV - SET DEV TO FALSE BEFORE COMMITTING'
 
-# open pages.json file
+# open pages.json file and generate html files for site
 with open('pages.json') as file:
     dict = json.load(file)
     pages = dict["pages"]
